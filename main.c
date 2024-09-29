9d38f4788d0fea74e2bf4388ac555aa295ca9358
Bobeica Nikita <nbobeica@DESKTOP-44P6421>
2024-09-29 20:43:44 +0200

    etape 4 : créer les conditions

--- main.c
+++ main.c
@@ -7,5 +7,63 @@
 
 int main()
 {
+	printf("Bonjour \n");
+	printf("Choissisez une operation: +, -, *, /, %% \n");
+
+	char symbol;
+	scanf("%c", &symbol);
+
+	printf(" Choissisez un nombre\n");
+	int x;
+
+        scanf("%d", &x);
+
+	printf("Choissisez un deuxieme nombre\n");
+	int y;
+
+        scanf("%d", &y);
+
+	if(symbol == '+')
+	{
+
+		printf("addition\n");
+
+	}
+
+	else if(symbol == '-' )
+	{
+
+		printf("soustraction\n");
+
+	}
+
+	else if(symbol == '*' )
+	{
+
+		printf("multiplication\n");
+
+	}
+
+	else if(symbol == '/' )
+	{
+
+		printf("division\n");
+
+	}
+
+	else if(symbol == '%' )
+	{
+
+		printf("modulo\n");
+
+	}
+
+	else
+	{
+
+        	printf("L'operateur n'est pas reconnu\n");
+	}
+
+	exit (0);
 
 }
